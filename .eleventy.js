2c2d777014f1b5a8737c13dad6b75f58c85af621
lgirs <l_girs@hotmail.com>
2025-08-19 00:20:39 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
-export default function(eleventyConfig) {
+// .eleventy.js
+export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
   return {
-    dir: { input: "src", includes: "_includes", output: "_site" },
-    passthroughFileCopy: true
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
   };
 }
